d475c86cdbce65913c8186e3db618378f7eb2cb7
李召君 <lilizhaojun@163.com>
2018-03-03 14:57:28 +0800

    测试test

---
+++ mypack/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/3 下午2:56
+# @Author  : lizhaojun
+# @Site    : 
+# @File    : test.py
+# @Software: PyCharm
+
+print("test------test")
